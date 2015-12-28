999adc3fa0b9edd36701950e02a1bcd7c49f1577
Michael Ruane <mruane@dvrpc.org>
2015-12-28 13:00:23 -0500

    Revisions
    Added minor changes to your previous version of the code with comments

--- setyear.py
+++ setyear.py
@@ -1,16 +1,23 @@
-
 import arcpy
-import datetime
+from datetime import datetime 	#import the datetime class from the datetime module
 
 tc = "d:\\temp\\tc.shp"
+fields = ["SETDATE", "SETYEAR"]
 
-##search cursor list of dates where setyear is blank
-dates = [i for i, in arcpy.da.SearchCursor
-       (tc,"SETDATE",'CHAR_LENGTH("SETYEAR")=1') if i != None]
+## either of these clauses should work if you are testing for a field not set
+where = 'char_length("SETYEAR") = 1'
+#where = '"SETYEAR"' + " < '1900'"
 
-for date in dates:
-    date.strftime("%Y")
-    print date
+with arcpy.da.UpdateCursor(tc, fields, where) as yearcursor:
+	for row in yearcursor:
+		set_date = row[0] 	#get the SETDATE field value
+		
+		set_year = datetime.strftime(set_date, "%Y")  	#use datetime to create string of year 
+
+		## add your udpate field/row functionality
+
+del yearcursor
+
 
 ##
 ##with arcpy.da.UpdateCursor(tc,["SETDATE","SETYEAR"],'CHAR_LENGTH("SETYEAR")=1') as yearcursor:
